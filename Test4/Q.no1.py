2555634a903dd36e8e92dd803c7acb7c784a1e65
Priyanka11-06 <priyankapatil57742@gmail.com>
2025-08-20 23:23:59 +0530

    Add files via upload

---
+++ Test4/Q.no1.py
@@ -0,0 +1,11 @@
+def factorial(num):
+    if num == 0 or num == 1:
+        return 1
+    else:
+        return num * factorial(num-1)
+
+        
+num = int(input("Enter a number"))
+print(f"factorial of{num} is:", factorial(num))
+
+    
